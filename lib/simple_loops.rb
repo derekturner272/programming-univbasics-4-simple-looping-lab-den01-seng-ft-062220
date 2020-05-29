49f5d0a21247332833f558557b650ab776fecf0b
Derek Turner <turner.derek.b@gmail.com>
2020-05-29 19:28:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,3 +15,12 @@ def loop_message_n_times(message, n)
     counter += 1 
   end
 end
+
+def output_array(array)
+  counter = 0 
+
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
